4c682634a623e8a47c94c303a155d536d2810ff8
mgaskins17 <mattgaskins77@gmail.com>
2022-11-23 09:21:38 -0700

    Fresh commit, forgot where I left off

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
  const needengineer = require('./lib/Engineer');
  const needintern = require('./lib/Intern');
  const cardHTML = require('./dist/cardHTML');
- const generateHTML = require('./dist/generateHTML');
 
 // initializing constants
 let manager = [];
